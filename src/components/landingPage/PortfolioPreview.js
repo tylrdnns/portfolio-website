ae114e07a1e51c5a76b91d5716addc25bd03c1f0
tylrdnns <68363565+tylrdnns@users.noreply.github.com>
2022-08-31 14:46:52 -0500

    reroute more projects button

--- src/components/landingPage/PortfolioPreview.js
+++ src/components/landingPage/PortfolioPreview.js
@@ -30,7 +30,7 @@ const PortfolioPreview = () => {
         <Link
           onClick={ResetLocation}
           className="more-projects-btn pink-text"
-          to="/personal-portfolio/portfolio"
+          to="/portfolio"
         >
           More projects
         </Link>
