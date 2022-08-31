9f7565b3a06a5d8be7af457970ffaf38e5a0d5bd
tylrdnns <68363565+tylrdnns@users.noreply.github.com>
2022-08-31 14:37:49 -0500

    further changes to portfolio websites

--- src/components/data/ProjectsData.js
+++ src/components/data/ProjectsData.js
@@ -1,21 +1,14 @@
-import PizzaTime from "../../images/PizzaTime.jpeg";
-import TravelWithCatherine from "../../images/TravelWithCatherine.jpeg";
-import GuessMyNumber from "../../images/GuessMyNumber.jpeg";
-import Forkify from "../../images/Forkify.jpeg";
-import Edgy from "../../images/Edgy.jpeg";
-import RestCountries from "../../images/RestCountries.jpeg";
-import Bankist from "../../images/Bankist.jpeg";
-import FrontendMentorSolutions from "../../images/FrontendMentorSolutions.jpeg";
 import entlyExample from "../../images/ently_example.png";
 import promoquoExample from "../../images/promoquo_example.png";
 import okayipullupExample from "../../images/okayipullup_example.png";
+import portfolioExample from "../../images/portfolioWebsite_example.png";
 
 const ProjectsPreviewData = [
   {
     id: 1,
     img: okayipullupExample,
     name: "okayipullup",
-    stack: ["< Javascript />", "< React.js />", "< CSS3 />", "< MongoDB />", "< AWS />"],
+    stack: ["< Javascript />", "< React.js />", "< CSS3 />", "< MongoDB />", "< AWS />", "< RESTful API />"],
     src: "https://www.okayipullup.com/",
     source: "https://github.com/tylrdnns/okayipullup",
     description:
@@ -25,7 +18,7 @@ const ProjectsPreviewData = [
     id: 2,
     img: entlyExample,
     name: "Ently",
-    stack: ["< Javascript />", "< Solidity />", "< React.js />", "< GraphQL />", "< IPFS />", "< CSS3 />"],
+    stack: ["< Javascript />", "< Solidity />", "< React.js />", "< GraphQL />", "< IPFS />", "< CSS3 />", "< RESTful API />"],
     src: "https://entlymarketplace.io/",
     source: "https://github.com/tylrdnns/ently",
     description:
@@ -35,11 +28,21 @@ const ProjectsPreviewData = [
     id: 3,
     img: promoquoExample,
     name: "Promoquo",
-    stack: ["< RESTful API />", "< CSS3 />", "< React.js />"],
+    stack: ["< Javascript />", "< RESTful API />", "< CSS3 />", "< React.js />"],
     src: "https://promoquo.com/",
     source: "https://github.com/tylrdnns",
     description:
       "The efficiency and effectiveness of marketing through social media today is hindered by a huge gap in facilitating business online. Specifically for collaborations between brands and creators there is no platform to automate the process of content marketing that both parties enjoy. By providing a secure way to both collaboarate and compensate users, Promoquo serves as an essential part of handling online promotions.",
+  },
+  {
+    id: 4,
+    img: portfolioExample,
+    name: "Personal Portfolio Website",
+    stack: ["UI/UX", "< CSS3 />", "< React.js />"],
+    src: "https://tylrdnns.xyz/",
+    source: "https://github.com/tylrdnns",
+    description:
+      "This is a personal portfolio website that exemplifies fluid user experience while content properly conveying the essential subject matter",
   },
 ];
 
@@ -73,6 +76,16 @@ const ProjectsData = [
     source: "https://github.com/tylrdnns",
     description:
       "The efficiency and effectiveness of marketing through social media today is hindered by a huge gap in facilitating business online. Specifically for collaborations between brands and creators there is no platform to automate the process of content marketing that both parties enjoy. By providing a secure way to both collaboarate and compensate users, Promoquo serves as an essential part of handling online promotions.",
+  },
+  {
+    id: 4,
+    img: portfolioExample,
+    name: "Personal Portfolio Website",
+    stack: ["UI/UX", "< CSS3 />", "< React.js />"],
+    src: "https://tylrdnns.xyz/",
+    source: "https://github.com/tylrdnns",
+    description:
+      "This is a personal portfolio website that exemplifies fluid user experience while content properly conveying the essential subject matter",
   },
 ];
 
